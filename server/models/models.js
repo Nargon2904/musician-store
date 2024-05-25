ebacd2b92de6f4b3fa87391a802c518d0513bca8
AnthonyNargon2904 <antoga2004@yandex.ru>
2024-05-25 12:42:12 +0300

    started

---
+++ server/models/models.js
@@ -0,0 +1,55 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true,},
+    fio: {type: DataTypes.STRING},
+    address: {type: DataTypes.STRING, allowNull: false},
+    phone: {type: DataTypes.INTEGER},
+    role: {type: DataTypes.STRING, defaultValue: "USER"},
+})
+
+const Basket = sequelize.define('basket', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const BasketProduct = sequelize.define('basket_product', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Product = sequelize.define('product', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    img: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.STRING},
+})
+
+const Type = sequelize.define('type', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    size: {type: DataTypes.INTEGER, defaultValue: 0},
+})
+
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+Basket.hasMany(BasketProduct)
+BasketProduct.belongsTo(Basket)
+
+Product.hasMany(BasketProduct)
+BasketProduct.belongsTo(Product)
+
+Type.hasMany(Product)
+Product.belongsTo(Type)
+
+
+module.exports = {
+    User,
+    Basket,
+    BasketProduct,
+    Product,
+    Type
+}
